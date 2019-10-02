1211c6945f96d1e405c8c62e804746b8fc7c9c6b
sandrasiby <sandrasiby@gmail.com>
2019-10-02 17:47:35 +0200

    test pages

---
+++ test/test_pages/get_cookie.js
@@ -0,0 +1,2 @@
+var x = document.cookie;
+console.log(x);
---
+++ test/test_pages/test_simple_script.js
@@ -0,0 +1,12 @@
+
+var div_element = document.createElement("div");
+div_element.innerHTML = 'Test Page';
+document.body.appendChild(div_element);
+
+var button = document.createElement("button");
+button.innerHTML = "Hello";
+document.body.appendChild(button);
+
+var newimg = document.createElement("img");
+newimg.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgrjE6kteaw0qJ2jeeeeOUyR_-qb-DQ9ke3j33pS_dE0vvQXDR";
+document.body.appendChild(newimg);
